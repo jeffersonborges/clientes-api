cb5a8ac638f1c82c4629b79473955bac60ce353b
jeffersonborges <jefferson.gmb@gmail.com>
2024-02-20 11:49:23 -0300

    Validação dos campos

--- src/main/kotlin/br/com/jborges/clientesapi/models/ClienteModel.kt
+++ src/main/kotlin/br/com/jborges/clientesapi/models/ClienteModel.kt
@@ -1,14 +1,16 @@
 package br.com.jborges.clientesapi.models
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 
 @Entity(name = "clientes")
 data class ClienteModel (
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
-    val nome: String,
-    val telefone: String,
-    val endereco: String
+    @Column(nullable = false, length = 80) val nome: String,
+    @Column(nullable = false, length = 14) val telefone: String,
+    @Column(nullable = false, length = 150) val endereco: String
 )
